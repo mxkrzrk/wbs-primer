bddf1594f922b972942c657a98008b7bcaa42462
Massimilianok <massimiliano.rizzuto87@gmail.com>
2020-12-30 15:54:03 +0100

    Create main section for the list of articles
    I use an array js to create the contents of articles, then I cycle it to create dynamically the articles.

---
+++ src/data/data.js
@@ -0,0 +1,34 @@
+export const blogArticle = [
+  {
+    mission: 'WAC Corporal',
+    year: '1946',
+    description:
+      'It was the first (US designed) rocket that reached the edge of space.',
+    links: {
+      wiki: 'https://en.wikipedia.org/wiki/WAC_Corporal',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/f/f6/Frank_Malina_with_WAC_Corporal_rocket_at_White_Sands.jpg',
+    },
+  },
+  {
+    mission: 'V-2',
+    year: '1946',
+    description:
+      'The first pictures of the Earth were taken from an altitude of 105 km.',
+    links: {
+      wiki: 'https://en.wikipedia.org/wiki/V-2_rocket',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/6/6d/Fus%C3%A9e_V2.jpg',
+    },
+  },
+  {
+    mission: 'R-1',
+    year: '1951	',
+    description: 'First time dogs were sent to space.',
+    links: {
+      wiki: 'https://en.wikipedia.org/wiki/R-1_(missile)',
+      image:
+        'https://upload.wikimedia.org/wikipedia/commons/4/43/1-%D0%BF%D1%83%D1%81%D0%BA%D1%83-%D0%BF%D0%BE%D1%81%D0%B2%D1%8F%D1%89%D0%B0%D0%B5%D1%82%D1%81%D1%8F_1S.jpg',
+    },
+  },
+];
--- src/index.js
+++ src/index.js
@@ -1,5 +1,44 @@
 import './style/index.css';
 import './style/header.css';
+import './style/main.css';
 import './style/footer.css';
+import { blogArticle } from './data/data';
 
-console.log('Hello World');
+function createSection(mission, year, description, wiki, image) {
+  const section = document.createElement('section');
+  section.classList.add('section');
+  section.innerHTML = `
+  <article class="section-article">
+    <div class="section-article-image-box">
+      <img
+        src="${image}"
+        alt="image"
+        class="section-article-image"
+      />
+    </div>
+    <div class="section-article-text">
+      <div class="section-article-text-info">
+        <h2>${mission}</h2>
+        <span>${year}</span>
+        <a href="${wiki}" target="_blank"></a>
+      </div>
+      <p>
+      ${description}
+      </p>
+    </div>
+  </article>
+`;
+  return section;
+}
+
+const main = document.getElementById('main');
+blogArticle.map((el) => {
+  const article = createSection(
+    el.mission,
+    el.year,
+    el.description,
+    el.links.wiki,
+    el.links.image
+  );
+  return main.appendChild(article);
+});
